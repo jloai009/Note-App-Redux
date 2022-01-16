704f34f941235e1043a87b3d67e86513513879cf
Jose <jloai009@fiu.edu>
2022-01-16 10:39:34 -0500

    Add filterReducer and its action creator

---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const filterReducer = (state = 'ALL', action) => {
+  switch (action.type) {
+    case 'SET_FILTER':
+      return action.filter
+    default:
+      return state
+  }
+}
+
+export const filterChange = filter => {
+  return {
+    type: 'SET_FILTER',
+    filter,
+  }
+}
+
+export default filterReducer
