ec1f66455ec52ad530ed1e5d8bf259399d4296b5
Jose <jloai009@fiu.edu>
2022-01-16 03:39:52 -0500

    Add action creator functions

--- src/index.js
+++ src/index.js
@@ -31,21 +31,29 @@ const App = () => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    store.dispatch({
+    store.dispatch(createNote(content))
+  }
+
+  const createNote = (content) => {
+    return {
       type: 'NEW_NOTE',
       data: {
         content,
         important: false,
         id: generateId()
       }
-    })
+    }
+  }
+
+  const toggleImportanceOf = (id) => {
+    return {
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id }
+    }
   }
 
   const toggleImportance = (id) => {
-    store.dispatch({
-      type: 'TOGGLE_IMPORTANCE',
-      data: { id }
-    })
+    store.dispatch(toggleImportanceOf(id))
   }
 
   return (
