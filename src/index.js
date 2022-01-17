ce1bad765ff8b0991e1720ce588b98787822395e
Jose <jloai009@fiu.edu>
2022-01-16 23:40:55 -0500

    store uses composeWithDevTools

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools()
 )
 
 ReactDOM.render(
